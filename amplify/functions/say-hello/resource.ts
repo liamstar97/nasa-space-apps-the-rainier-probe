ad102839dcc5643f5ad6012500d19d8b91c37c72
Liam Thompson <liam.thompson@iointel.com>
2025-10-04 21:04:08 -0700

    making local lambda copy OS agnostic

--- amplify/functions/say-hello/resource.ts
+++ amplify/functions/say-hello/resource.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from "node:url";
 import { defineFunction } from "@aws-amplify/backend";
 import { DockerImage, Duration } from "aws-cdk-lib";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import fs from "fs-extra";
 
 const functionDir = path.dirname(fileURLToPath(import.meta.url));
 
@@ -16,13 +17,15 @@ export const sayHelloFunctionHandler = defineFunction(
       timeout: Duration.seconds(20), //  default is 3 seconds
       code: Code.fromAsset(functionDir, {
         bundling: {
-          image: DockerImage.fromRegistry("public.ecr.aws/docker/library/python:3.9.23-slim-trixie"), // replace with desired image from AWS ECR Public Gallery
+          image: DockerImage.fromRegistry("public.ecr.aws/docker/library/python:3.9.23-slim-trixie"),
           local: {
             tryBundle(outputDir: string) {
               execSync(
                 `python3 -m pip install -r ${path.join(functionDir, "requirements.txt")} -t ${path.join(outputDir)} --platform manylinux2014_x86_64 --only-binary=:all:`
               );
-              execSync(`cp -r ${functionDir}/* ${path.join(outputDir)}`);
+              fs.copySync(functionDir, outputDir, {
+                filter: (src) => !src.startsWith(outputDir), 
+              });
               return true;
             },
           },
