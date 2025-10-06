db78e8f109ee804e29ae90f108a764d09624172d
Liam Thompson <liam.thompson@iointel.com>
2025-10-05 20:22:26 -0700

    fixing npm packages

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,4 @@
-import { defineBackend } from '@aws-amplify/backend';
+import { defineBackend, secret } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { sayHelloFunctionHandler } from './functions/say-hello/resource';
@@ -8,9 +8,14 @@ import { earthaccessFunctionHandler } from './functions/earthaccess/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   sayHelloFunctionHandler,
   earthaccessFunctionHandler,
 });
+
+// Add secrets to earthaccessFunctionHandler environment variables
+backend.earthaccessFunctionHandler.addEnvironment('EARTHDATA_USERNAME', secret('EARTHDATA_USERNAME'));
+backend.earthaccessFunctionHandler.addEnvironment('EARTHDATA_PASSWORD', secret('EARTHDATA_PASSWORD'));
+
